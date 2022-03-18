b3be1eda57e3c4556ff675bc9fa3ffe49d97ce05
Andrew Lange <andrew.lange13@gmail.com>
2022-03-18 09:55:28 -0500

    completes join example exercise

--- join_example_exercise.sql
+++ join_example_exercise.sql
@@ -9,3 +9,20 @@ VALUES
     ('al', 'al@example.com', null),
     ('andrew', 'andrew@gmail.com', 2),
     ('nick', 'nick@aol.com', 2);
+
+SELECT users.name as user_name, roles.name as role_name
+FROM users
+         JOIN roles ON users.role_id = roles.id;
+
+SELECT users.name AS user_name, roles.name AS role_name
+FROM users
+         LEFT JOIN roles ON users.role_id = roles.id;
+
+SELECT users.name AS user_name, roles.name AS role_name
+FROM users
+         RIGHT JOIN roles ON users.role_id = roles.id;
+
+SELECT roles.name as role_name, COUNT(roles.name) AS 'users with role'
+FROM users
+         JOIN roles ON users.role_id = roles.id
+GROUP BY roles.name;
